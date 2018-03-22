8bf9ac4d6a4b43a5f4a0d600324b55d7d894bed0
wangjf <wangjinfeng>
2018-03-22 14:17:34 +0800

    init

--- ml/kNN.py
+++ ml/kNN.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from numpy import *
+from os import listdir
 import operator
 
 
@@ -108,3 +109,33 @@ def img2vector(filename):
         for j in range(32):
             returnVect[0, 32 * i + j] = int(lineStr[j])
     return returnVect
+
+
+# 手写数字识别系统的测试代码
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('/Users/wangjf/Downloads/machinelearninginaction/Ch02/digits/trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split(".")[0]
+        classNumStr = int(fileStr.split("_")[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector(
+            "/Users/wangjf/Downloads/machinelearninginaction/Ch02/digits/trainingDigits/%s" % fileNameStr)
+    testFileList = listdir("/Users/wangjf/Downloads/machinelearninginaction/Ch02/digits/testDigits")
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector(
+            '/Users/wangjf/Downloads/machinelearninginaction/Ch02/digits/testDigits/%s' % fileNameStr)
+        classifierResult = classify(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("the classifier came back with: %d,the real answer is: %d" % (classifierResult, classNumStr))
+        if classifierResult != classNumStr: errorCount += 1.0
+
+    print('\nthe total number of errors is: %d' % errorCount)
+    print('\nthe total error rate is: %f' % (errorCount / float(mTest)))
