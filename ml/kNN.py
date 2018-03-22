b6f048a07f93a95a1e1a5cec7548bcb018e7d606
wangjf <wangjinfeng>
2018-03-22 18:52:11 +0800

    init

--- ml/kNN.py
+++ ml/kNN.py
@@ -10,14 +10,27 @@ def createDataSet():
     return group, labels
 
 
+"""
+对于每一个在数据集中的数据点：
+    计算目标的数据点（需要分类的数据点）与该数据点的距离
+    将距离排序：从小到大
+    选取前K个最短距离
+    选取这K个中最多的分类类别
+    返回该类别来作为目标数据点的预测值
+"""
+
+
 # k-近邻算法
 def classify(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
+    #   距离度量，度量公式为欧式距离
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances ** 0.5
+    #   将距离排序：从小到大
     sortedDistIndicies = distances.argsort()
+    #   选取前K个最短距离，选取这K个中最多的分类类别
     classCount = {}
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
@@ -32,8 +45,10 @@ def classify(inX, dataSet, labels, k):
 # 将文本记录转化为NumPy的解析程序
 def file2matrix(filename):
     """
-    :param filename:
-    :return:
+    param:
+        filename:数据文件路径
+    return:
+        数据矩阵 returnMat 和对应的类别 classLabelVector
     from ml import kNN
     datingDataMat,datingLabels = kNN.file2matrix("/Users/wangjf/Downloads/machinelearninginaction/Ch02/datingTestSet2.txt")
 
@@ -46,41 +61,70 @@ def file2matrix(filename):
     """
     fr = open(filename)
     arrayOLines = fr.readlines()
+    #   获得文件中的数据行的行数
     numberOfLines = len(arrayOLines)
+    #   生成对应的空矩阵
     returnMat = zeros((numberOfLines, 3))
     classLabelVector = []
     index = 0
     for line in arrayOLines:
+        #   返回移除字符串头尾指定的字符生成的新字符串
         line = line.strip()
+        #   以'\t'切割字符串
         listFromLine = line.split('\t')
+        #   每列的属性数据
         returnMat[index, :] = listFromLine[0:3]
+        #   每列的类别数据，就是label标签数据
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
-
+    #   返回数据矩阵returnMat和对应的类别classLabelVector
     return returnMat, classLabelVector
 
 
 # 归一化特征值
 def autoNorm(dataSet):
+    """
+    :param dataSet:数据集
+    :return:
+        归一化后的数据集normDataSet,ranges和minVals即最小值与范围，并没有用到
+    归一化公式:
+        Y = (X-Xmin)/(Xmax-Xmin)
+        其中的 min 和 max 分别是数据集中的最小特征值和最大特征值。该函数可以自动将数字特征值转换为 0 到 1 的区间
+    """
+    #   计算每种属性的最大值，最小值，范围
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
+    #   极差
     ranges = maxVals - minVals
     normDataSet = zeros(shape(dataSet))
     m = dataSet.shape[0]
+    #   生成与最小值之差组成的矩阵
     normDataSet = normDataSet - tile(minVals, (m, 1))
+    #   将最小值之差除以范围组成矩阵
     normDataSet = normDataSet / tile(ranges, (m, 1))
     return normDataSet, ranges, minVals
 
 
 # 分类器针对约会网站的测试代码
 def datingClassTest():
-    hoRatio = 0.90
+    """
+    对约会网站的测试方法
+    :return:
+        错误数
+    """
+    #   设置测试数据的一个比例（训练数据集比例=1-hoRatio）
+    hoRatio = 0.1  # 测试范围，一部分测试一部分作为样本
+    #   从文本中加载数据
     datingDataMat, datingLabels = file2matrix('/Users/wangjf/Downloads/machinelearninginaction/Ch02/datingTestSet2.txt')
+    #   归一化数据
     normMat, ranges, minVals = autoNorm(datingDataMat)
+    #   m 表示数据的行数，即矩阵的第一维
     m = normMat.shape[0]
+    #   设置测试的样本数量，numTestVecs:m 表示训练样本的数量
     numTestVecs = int(m * hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
+        #   对数据测试
         classifierResult = classify(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
         print("the classifier came back with: %d,the real answer is: %d" % (classifierResult, datingLabels[i]))
         if classifierResult != datingLabels[i]: errorCount += 1.0
@@ -113,17 +157,22 @@ def img2vector(filename):
 
 # 手写数字识别系统的测试代码
 def handwritingClassTest():
+    #   导入训练数据
     hwLabels = []
     trainingFileList = listdir('/Users/wangjf/Downloads/machinelearninginaction/Ch02/digits/trainingDigits')
     m = len(trainingFileList)
     trainingMat = zeros((m, 1024))
+    #   hwLabels存储0~9对应的index位置，trainingMat存放的每个位置对应的图片向量
     for i in range(m):
         fileNameStr = trainingFileList[i]
         fileStr = fileNameStr.split(".")[0]
         classNumStr = int(fileStr.split("_")[0])
         hwLabels.append(classNumStr)
+        #   将 32*32 的矩阵->1*1024的矩阵
         trainingMat[i, :] = img2vector(
             "/Users/wangjf/Downloads/machinelearninginaction/Ch02/digits/trainingDigits/%s" % fileNameStr)
+
+    #   导入测试数据
     testFileList = listdir("/Users/wangjf/Downloads/machinelearninginaction/Ch02/digits/testDigits")
     errorCount = 0.0
     mTest = len(testFileList)
@@ -139,3 +188,22 @@ def handwritingClassTest():
 
     print('\nthe total number of errors is: %d' % errorCount)
     print('\nthe total error rate is: %f' % (errorCount / float(mTest)))
+
+
+"""
+k 值的选择
+    1、k 值的选择会对 k 近邻算法的结果产生重大的影响。
+    2、如果选择较小的 k 值，就相当于用较小的邻域中的训练实例进行预测，“学习”的近似误差（approximation error）会减小，只有与输入实例较近的
+    （相似的）训练实例才会对预测结果起作用。但缺点是“学习”的估计误差（estimation error）会增大，预测结果会对近邻的实例点非常敏感。如果邻近
+    的实例点恰巧是噪声，预测就会出错。换句话说，k 值的减小就意味着整体模型变得复杂，容易发生过拟合。
+    3、如果选择较大的 k 值，就相当于用较大的邻域中的训练实例进行预测。其优点是可以减少学习的估计误差。但缺点是学习的近似误差会增大。这时与输
+    入实例较远的（不相似的）训练实例也会对预测起作用，使预测发生错误。 k 值的增大就意味着整体的模型变得简单。
+    4、近似误差和估计误差，请看这里：https://www.zhihu.com/question/60793482
+    
+距离度量
+    1、特征空间中两个实例点的距离是两个实例点相似程度的反映。
+    2、k 近邻模型的特征空间一般是 n 维实数向量空间 向量空间 。使用的距离是欧氏距离，但也可以是其他距离，如更一般的 Lp距离 距离，或者 Minkowski 距离。
+
+分类决策规则
+    1、k 近邻算法中的分类决策规则往往是多数表决，即由输入实例的 k 个邻近的训练实例中的多数类决定输入实例的类。
+"""
---
+++ ml/trees.py
@@ -0,0 +1,189 @@
+# -*- coding:utf-8 -*-
+from math import log
+import operator
+
+"""
+熵（entropy）：
+    熵指的是体系的混乱的程度，在不同的学科中也有引申出的更为具体的定义，是各领域十分重要的参量。
+
+信息论（information theory）中的熵（香农熵）：
+    是一种信息的度量方式，表示信息的混乱程度，也就是说：信息越有序，信息熵越低。例如：火柴有序放在火柴盒里，熵值很低，相反，熵值很高。
+
+信息增益（information gain）： 
+    在划分数据集前后信息发生的变化称为信息增益。
+"""
+
+
+def calcShannonEnt(dataSet):
+    #   求list的长度，表示计算参与训练的数据量
+    numEntries = len(dataSet)
+    #   计算分类标签label出现的次数
+    labelCounts = {}
+    #   the the number of unique elements and their occurrence
+    for featVec in dataSet:
+        #   将当前实例的标签存储，即每一行数据的最后一个数据代表的是标签
+        currentLabel = featVec[-1]
+        #   为所有可能的分类创建字典，如果当前的键值不存在，则扩展字典并将当前键值加入字典。每个键值都记录了当前类别出现的次数。
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+
+    #   对于 label 标签的占比，求出 label 标签的香农熵
+    shannonEnt = 0.0
+    for key in labelCounts:
+        #   使用所有类标签的发生频率计算类别出现的概率。
+        prob = float(labelCounts[key]) / numEntries
+        #   计算香农熵，以 2 为底求对数
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+#   按照给定特征划分数据集
+def splitDataSet(dataSet, index, value):
+    """
+    splitDataSet(通过遍历dataSet数据集，求出index对应的colnum列的值为value的行)就是依据index列进行分类，如果index列的数据等于 value
+    的时候，就要将 index 划分到我们创建的新的数据集中
+    :param dataSet:
+        数据集，待划分的数据集
+    :param index:
+        表示每一行的index列，划分数据集的特征
+    :param value:
+        表示index列对应的value值，需要返回的特征的值
+    :return:
+        index列为value的数据集[该数据集需要排除index列]
+    """
+    retDataSet = []
+    for featVec in dataSet:
+        #   index列为value的数据集[该数据集需要排除index列]
+        #   判断index列的值是否为value
+        if featVec[index] == value:
+            #   chop out index used for splitting
+            #   [:index]表示前index行，即若index为2，就是取featVec的前index行
+            reducedFeatVec = featVec[:index]
+            '''
+            请百度查询一下：extend和append的区别
+                music_media.append(object) 向列表中添加一个对象object
+                music_media.extend(sequence) 把一个序列seq的内容添加到列表中 (跟 += 在list运用类似， music_media += sequence)
+            1、使用append的时候，是将object看作一个对象，整体打包添加到music_media对象中。
+            2、使用extend的时候，是将sequence看作一个序列，将这个序列和music_media序列合并，并放在其后面。
+            music_media = []
+            music_media.extend([1,2,3])
+            print music_media
+            # 结果：
+            # [1, 2, 3]
+            
+            music_media.append([4,5,6])
+            print music_media
+            # 结果：
+            # [1, 2, 3, [4, 5, 6]]
+            
+            music_media.extend([7,8,9])
+            print music_media
+            # 结果：
+            # [1, 2, 3, [4, 5, 6], 7, 8, 9]
+            '''
+            reducedFeatVec.extend(featVec[index + 1:])
+            #   [index+1:]表示从跳过 index 的 index+1 行，取接下来的数据
+            #   收集结果值，index列为 value 的行[该行需要排除index列]
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+#   按照最好的数据集划分方式
+def chooseBestFeatureToSplit(dataSet):
+    """
+        chooseBestFeatureToSplit(选择最好的特征)
+    :param dataSet:
+        数据集
+    :return:
+        bestFeature 最优的特征列
+    """
+    #   求第一行有多少列的Feature，最后一列是label列
+    numFeatures = len(dataSet[0]) - 1
+    #   数据集的原始信息熵
+    baseEntropy = calcShannonEnt(dataSet)
+    #   最优的信息增益值，和最优的Feature编号
+    bestInfoGain, bestFeature = 0.0, -1
+    #   iterate over all the features
+    for i in range(numFeatures):
+        #   create a list of all the examples of this feature
+        #   获取对应的feature下的所有数据
+        featList = [example[i] for example in dataSet]
+        #   get a set of unique values
+        #   获取剔重合的集合，使用set对list数据进行去重
+        uniqueVals = set(featList)
+        #   创建一个临时的信息熵
+        newEntropy = 0.0
+        #   遍历某一列的value集合，计算该列的信息熵
+        #   遍历当前特征中的所有唯一属性值，对每个唯一属性值划分一次数据集，计算数据集的新熵值，并对所有唯一特征值得到的熵求和
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            #   计算概率
+            prob = len(subDataSet) / float(len(dataSet))
+            #   计算信息熵
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        #   gain[信息增益]：划分数据集前后的信息变化，获取信息熵最大的值
+        #   信息增益是熵的减少或者是数据无序度的减少。最后，比较所有特征中的信息增益，返回最好特征划分的索引值
+        infoGain = baseEntropy - newEntropy
+        print("infoGain = ", infoGain, "bestFeature = ", i, baseEntropy, newEntropy)
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+"""
+    问：上面的 newEntropy 为什么是根据子集计算的呢？
+    答：因为我们在根据一个特征计算香农熵的时候，该特征的分类值是相同，这个特征这个分类的香农熵为 0；这就是为什么计算新的香农熵的时候使用的是子集。
+"""
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    #   如果数据集的最后一列的第一个值出现的次数=整个集合的数量，也就是只有一个类别，就直接返回结果就行
+    #   第一个停止条件：所有的类标签完全相同，则直接返回该类标签
+    #   count()函数是统计括号中的值在list中出现的次数
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    #   如果数据集还有1列，那么最初出现label次数最多的一类，作为结果
+    #   第二个停止条件：使用完了所有特征，仍然不能将数据集划分成仅包含唯一类别的分组
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    #   选择最优的列，得到最优列对应的label含义
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    #   获取label的名称
+    bestFeatLabel = labels[bestFeat]
+    #   初始化myTree
+    myTree = {bestFeatLabel: {}}
+    #   注：labels列表是可变对象，在Python函数中作为参数时传值引用，能够被全局修改
+    #   所以这行代码导致函数外的同名变量被删除了元素，造成例句无法执行，提示'no surfacing' is not in list
+    del (labels[bestFeat])
+    #   取出最优列，然后它的branch做分类
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        #   求出剩余的标签label
+        subLabels = labels[:]
+        #   遍历当前选择特征包含的所有属性值，在每个数据集划分上递归调用函数createTree()
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+        #   print 'myTree',value,myTree
+    return myTree
