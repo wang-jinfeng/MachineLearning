ebea8e6a636fb577e4625064d93b3eb0804378f6
wangjf <wangjinfeng>
2018-04-17 18:32:01 +0800

    init

--- spark/cmd.py
+++ spark/cmd.py
@@ -8,20 +8,20 @@ app = Flask(__name__)
 @app.route("/userdesk", methods=['POST', 'GET'])
 def userdesk():
     if request.method == "POST":
-        print 'call post now'
+        print('call post now')
         id = request.args.get('id')
         data = request.args.get('data')
-        print id
-        print data
+        print(id)
+        print(data)
         status = os.system('echo ' + id + " " + data)
         return str(status)
 
     if request.method == "GET":
-        print 'call get now'
+        print('call get now')
         id = request.args.get('id')
         data = request.args.get('data')
-        print id
-        print data
+        print(id)
+        print(data)
         status = os.system('echo ' + id + " " + data)
         return str(status)
 
