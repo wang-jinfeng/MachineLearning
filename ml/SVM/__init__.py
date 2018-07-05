7eb7762849fdd4388fb7ee448565974d57827a12
wangjf <wangjinfeng>
2018-07-05 16:07:03 +0800

    init

---
+++ ml/SVM/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    Time    : 2018/7/5 下午4:01
+    Author  : wangjf
+    File    : __init__.py.py
+    GitHub  : https://github.com/wjf0627
+"""
