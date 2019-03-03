68d94aa80c4b684d9f5546fdf317a33e59b38c1c
jinfeng.wang <jinfeng.wang@mobvista.com>
2019-03-03 20:44:57 +0800

    update

--- ml/KNN/kNN.py
+++ ml/KNN/kNN.py
@@ -151,7 +151,7 @@ def img2vector(filename):
 	returnVect = zeros((1, 1024))
 	fr = open(filename)
 	for i in range(32):
-		lineStr = list(fr.readline())
+		lineStr = fr.readline()
 		for j in range(32):
 			returnVect[0, 32 * i + j] = int(lineStr[j])
 	return returnVect
